d991afa66220f93f5c408c4a9087b88ee8445c74
frankzhou <2280610818@qq.com>
2023-01-15 15:38:37 +0800

    新增Redis工具类，修改配置yml配置文件

--- src/main/java/com/frankzhou/comment/redis/RedisCache.java
+++ src/main/java/com/frankzhou/comment/redis/RedisCache.java
@@ -1,10 +1,30 @@
 package com.frankzhou.comment.redis;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 /**
  * @author This.FrankZhou
  * @version 1.0
- * @description 封装各种数据类型的redisTemplate操作
+ * @description 缓存工具类 设置逻辑过期时间 解决缓存击穿问题
  * @date 2023-01-14
  */
+@Slf4j
+@Component
 public class RedisCache {
+
+    private StringRedisTemplate stringRedisTemplate;
+
+    private final ExecutorService CACHE_REBUILD_EXECUTOR = Executors.newFixedThreadPool(10);
+    public RedisCache(StringRedisTemplate stringRedisTemplate) {
+        this.stringRedisTemplate = stringRedisTemplate;
+    }
+
+
 }
--- src/test/java/com/frankzhou/comment/CommentSystemApplicationTests.java
+++ src/test/java/com/frankzhou/comment/CommentSystemApplicationTests.java
@@ -3,11 +3,19 @@ package com.frankzhou.comment;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+/**
+* @Author this.FrankZhou
+* @Description 测试环境配置是否正确
+* @DateTime 2023/1/15 15:33
+*/
 @SpringBootTest
 class CommentSystemApplicationTests {
 
     @Test
     void contextLoads() {
+        String notice = "当配置多环境yml文件时，必须在主环境配置application.yml中，配好启动的环境，才能使用单元测试";
+        System.out.println(notice);
+        System.out.println("加载环境成功!");
     }
 
 }
