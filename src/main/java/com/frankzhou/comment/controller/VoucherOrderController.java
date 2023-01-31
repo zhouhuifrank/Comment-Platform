fb5f10e975fa6618ce7c194c5676e324967df5bd
frankzhou <2280610818@qq.com>
2023-01-31 20:00:46 +0800

    update 新增后台管理接口

--- src/main/java/com/frankzhou/comment/controller/VoucherOrderController.java
+++ src/main/java/com/frankzhou/comment/controller/VoucherOrderController.java
@@ -1,14 +1,14 @@
 package com.frankzhou.comment.controller;
 
 import com.frankzhou.comment.common.ResultDTO;
+import com.frankzhou.comment.dto.VoucherOrderQueryDTO;
+import com.frankzhou.comment.entity.VoucherOrder;
 import com.frankzhou.comment.service.IVoucherOrderService;
 import com.frankzhou.comment.service.IVoucherService;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * @author This.FrankZhou
@@ -26,5 +26,17 @@ public class VoucherOrderController {
     @PostMapping("/seckill/{id}")
     public ResultDTO<Long> getSeckillOrder(@PathVariable("id") Long voucherId) {
         return voucherOrderService.getSeckillOrder(voucherId);
+    }
+
+    // ---------------------后台管理接口--------------------
+
+    @GetMapping("/{id}")
+    public ResultDTO<List<VoucherOrder>> getVoucherOrderById(@PathVariable("id") Long VoucherId) {
+        return null;
+    }
+
+    @PostMapping("/page")
+    public ResultDTO<List<VoucherOrder>> getVoucherOrderByPage(@RequestBody VoucherOrderQueryDTO queryDTO) {
+        return null;
     }
 }
---
+++ src/main/java/com/frankzhou/comment/dto/ShopQueryDTO.java
@@ -0,0 +1,13 @@
+package com.frankzhou.comment.dto;
+
+import lombok.Data;
+
+/**
+ * @author This.FrankZhou
+ * @version 1.0
+ * @description 商品分页查询对象
+ * @date 2023-01-31
+ */
+@Data
+public class ShopQueryDTO extends PageQueryDTO {
+}
